13fec820d72efb2204985cf543ef56ae56668d68
HigorAnjos <38214470+HigorAnjos@users.noreply.github.com>
2019-02-07 15:07:25 -0200

    Add files via upload

---
+++ Metodos de Ordenacao/insertionSort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+void INSERTIONSORT (int *vet)
+{
+	int N = sizeof(vet)/sizeof(int); // tamanho do vetor
+	
+	for (int i = 2; i<N; i++)
+	{
+		
+	}
+	
+}
+
+int main ()
+{
+	int vet[5]={1,2,5,3,4};
+	
+	
+}
