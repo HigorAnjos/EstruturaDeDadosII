8e43400a4b624efb2a8ec2d549eba872d034941b
HigorAnjos <38214470+HigorAnjos@users.noreply.github.com>
2019-02-13 14:20:41 -0200

    Update insertionSort.cpp
    Me parece que nao precisa de dois auxiliares, e o metodo que usei para descobrir o tamanho do vetor nao funciona dentro de uma funcao

--- Metodos de Ordenacao/insertionSort.cpp
+++ Metodos de Ordenacao/insertionSort.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
-void INSERTIONSORT (int *vet)
+void INSERTIONSORT (int* vet, int T)
 {
-	int N = sizeof(vet)/sizeof(int); // tamanho do vetor
+	//int N = sizeof(vet)/ sizeof(int); // tamanho do vetor (Nao esta funcionando na funcao)
+	//std::cout<<"T= "<<N<<"\n";
 	
-	for (int i = 2; i<N; i++)
+	int aux, aux2;
+	int j=0;
+	for (int i = 1; i<T; i++)
 	{
-		
+		aux = vet[i];
+		for (j=i; j>0 && vet[j]<vet[j-1]; j--)
+		{
+			aux2 = vet[j];
+			vet[j] = vet[j-1];
+			vet[j-1] = aux2;
+		}
+		vet[j] = aux;
 	}
 	
 }
 
 int main ()
 {
-	int vet[5]={9,2,5,-1,4};
+	int T = 6;
+	int vet[T]={25,1,7,5,3,9};
 	
-	INSERTIONSORT (vet);
+	INSERTIONSORT (vet , T);
 	
-	
-	for (int i = 0;  i<5;  i++)
+	for (int i = 0;  i<T;  i++)
 	{
 		std::cout<<vet[i]<<" ";
 	}
+	
+	/*int N = sizeof(vet)/ sizeof(int);
+	std::cout<<"T= "<<N<<"\n";*/
 	
 	
 }
