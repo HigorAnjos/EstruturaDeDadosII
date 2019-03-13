8a9b101f982cf9a7070b60a30c9e362c487de074
HigorAnjos <38214470+HigorAnjos@users.noreply.github.com>
2019-03-13 11:46:08 -0300

    Update heap.cpp

--- Metodos de Ordenacao/heap.cpp
+++ Metodos de Ordenacao/heap.cpp
@@ -1,22 +1,19 @@
-
 #include <iostream>
 #include <algorithm>    // std::swap
 using namespace std;
 
-int vet[7]={2,7,5,10,4,3,90};
+int vet[7]={2,7,5,1,4,3,9};
 
 void Max(int* vet, int tamanho, int i=1)
 {
     int L = 2*i;
     int R = 2*i+1;
     
-    cout<<"L-> "<<L<<" R-> "<<R<<" I-> "<<i<<endl;
-    puts(" ");
-    
- if (i<tamanho/2)
- {
+	
 	if(L <= 7)
-	{
+	{   
+		 cout<<"L-> "<<L<<" R-> "<<R<<" I-> "<<i<<endl;
+	     puts(" ");
 		Max(vet, tamanho, L);
 		
 		if ( vet[i-1]<vet[2*i-1])
@@ -24,8 +21,12 @@ void Max(int* vet, int tamanho, int i=1)
 			swap(vet[i-1],vet[2*i-1]);
 		}
 	}
-	if (R <= 6)
+	
+	if (R <= 7)
 	{
+		 cout<<"2L-> "<<L<<" R-> "<<R<<" I-> "<<i<<endl;
+			puts(" ");
+		
 		Max(vet, tamanho, R);
 		
 		if (vet[i-1]<vet[2*i])
@@ -33,8 +34,7 @@ void Max(int* vet, int tamanho, int i=1)
             swap(vet[i-1],vet[2*i]);
 		}
 	}
-}	
-    
+
 }
 
 int main ()
